eb60fdce28b7a38a6f3392b351271b11b48e31ff
Dwaragnath41 <86316150+Dwaragnath41@users.noreply.github.com>
2024-01-14 10:26:13 +0530

    JS Basics
    Initial commit & features

---
+++ 01_learn.js
@@ -0,0 +1,28 @@
+var firstName = "Dwaragnath"
+console.log(typeof firstName);
+
+var companyName = "Persistent Systems"
+console.log(typeof companyName);
+
+var mobileNumber = 9944595955
+console.log(typeof mobileNumber);
+
+var isAutomation = true
+console.log(typeof isAutomation);
+
+var hasPlaywright;
+console.log(hasPlaywright);
+
+const browserName = "Chrome"
+
+function getBrowserVersion(){
+
+    if(browserName == "Chrome"){
+
+        let browserVersion = 6.0
+    }
+
+    console.log(browserVersion);
+}
+
+getBrowserVersion();
